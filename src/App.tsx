3654ac9be85da30571e3622a9fffb5bba414108c
tarunsaipanchala <tarun.panchala@xcubelabs.com>
2022-12-04 11:09:53 +0530

    static-pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import {ThemeProvider } from '@mui/material/styles';
 import {theme} from './theme';
+
 import Login from './app/Connects/PreLogin/Login/Login';
 import Register from './app/Connects/PreLogin/Register/Register';
 import Dashboard from './app/Connects/PostLogin/Dashboard/Dashboard';
